a7e5f44e65c0ee9789d8432a7955276dec80261c
Denis <deniskozlov@live.ru>
2015-12-02 18:48:14 +0300

    Initial commit

---
+++ common-ui-automator/bin/classes.dex.d
@@ -0,0 +1,3 @@
+C:\Users\Sash\Desktop\android-workspace\common-ui-automator\bin\classes.dex : \
+C:\Users\Sash\Desktop\android-workspace\common-ui-automator\bin\classes\com\bytewerk\uap\common\CommonCase.class \
+C:\Users\Sash\Desktop\android-workspace\common-ui-automator\bin\classes\CommonCase.class \
---
+++ common-ui-automator/src/com/bytewerk/uap/common/YandexTaxiWorker.java
@@ -0,0 +1,7 @@
+package com.bytewerk.uap.common;
+
+import android.support.test.uiautomator.UiAutomatorTestCase;
+
+public class YandexTaxiWorker extends UiAutomatorTestCase {
+
+}
